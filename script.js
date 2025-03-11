3bd4be4d759ad33569be76afb7a80b411e31bda8
Natalia <zezulinan27@gmail.com>
2025-03-11 09:11:41 +0300

    hometask-11
     Счетчик отображает количество правильно введенных слов и потом отображается второе слово и т.д
    Когда будет 5 правильно введенных или 5 неправильно введенных - тогда сообщение и конец игры

--- script.js
+++ script.js
@@ -9,26 +9,18 @@ function getRandomWord() {
     return words[Math.floor(Math.random() * words.length)];
 }
 
-
 function initGame() {
     currentWord = getRandomWord();
     currentCharIndex = 0;
-    correctCount = 0;
-    wrongCount = 0;
     mistakesCount = 0;
 
-
     renderWord();
-
-
     updateStatus();
 }
-
 
 function renderWord() {
     const wordContainer = document.querySelector('.word');
     wordContainer.innerHTML = '';
-
 
     for (let i = 0; i < currentWord.length; i++) {
         const charSpan = document.createElement('span');
@@ -53,34 +45,44 @@ document.addEventListener('keydown', (event) => {
     const pressedKey = event.key;
 
     if (pressedKey === currentWord[currentCharIndex]) {
-
-        correctCount++;
         currentCharIndex++;
-
 
         if (currentCharIndex >= currentWord.length) {
 
+            correctCount++;
             const spans = document.querySelectorAll('.word span');
             spans.forEach(span => span.classList.add('c'));
 
 
-            setTimeout(() => {
-                alert('Вы ввели слово правильно!');
+            if (correctCount === 5) {
+                alert('Поздравляем! Вы ввели 5 правильных слов. Игра окончена!');
+                resetGame();
+            } else {
                 initGame();
-            }, 500);
+            }
         } else {
             renderWord();
         }
     } else {
-
         mistakesCount++;
-        wrongCount++;
         const spans = document.querySelectorAll('.word span');
         spans[currentCharIndex].classList.add('w');
-    }
 
+
+        if (currentCharIndex === currentWord.length - 1) {
+            wrongCount++;
+
+            if (wrongCount === 5) {
+                alert(`Вы ввели ${wrongCount} неправильных слов. Игра окончена!`);
+                resetGame();
+            } else {
+                initGame();
+            }
+        }
+    }
 
     updateStatus();
 });
+
 
 window.onload = initGame;
